33a3de66650128204c75990ff39ec6a042be8bd7
Ashish Shah <eiepixxstuff@gmail.com>
2025-11-11 05:11:53 +0530

    feat: refactor Razorpay order creation logic and improve error handling

--- app/api/razorpay/order/route.ts
+++ app/api/razorpay/order/route.ts
@@ -1,34 +1,39 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import Razorpay from "razorpay";
 
 export async function POST(req: Request) {
     try {
         const { amount } = await req.json();
 
-        const razorpay = new Razorpay({
+        const razorpayClient = new Razorpay({
             key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
             key_secret: process.env.RAZORPAY_KEY_SECRET,
         });
 
-        const options = {
+        const paymentOptions = {
             amount: amount * 100,
             currency: "INR",
             receipt: `receipt_${Date.now()}`,
+            notes: {
+                purpose: "Just for fun I guess, BTW thank you!",
+            },
         };
 
-        const order = await razorpay.orders.create(options);
-        return NextResponse.json({
-            order,
-        });
-    } catch (error) {
-        console.log(`Error in razorpay order ${error}`);
+        const order = await razorpayClient.orders.create(paymentOptions);
+
         return NextResponse.json(
             {
-                error: "Faild to create order",
+                order,
             },
+            { status: 200 }
+        );
+    } catch (error) {
+        console.log(`Something went wrong in order API, check it out.`);
+        return NextResponse.json(
             {
-                status: 500,
-            }
+                error: "Failed to create order",
+            },
+            { status: 500 }
         );
     }
 }
